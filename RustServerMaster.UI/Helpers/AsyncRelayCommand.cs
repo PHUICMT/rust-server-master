1f6c2b8746f7b1cc9ec4ebdf156e7cb14636194b
PHUICMT <phuicmt@odds.team>
2025-06-04 21:24:03 +0700

    Add InverseBoolConverter, AsyncRelayCommand, RelayCommand, and BaseViewModel classes

---
+++ RustServerMaster.UI/Helpers/AsyncRelayCommand.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace RustServerMaster.Helpers
+{
+    /// <summary>
+    /// ICommand implementation for asynchronous Task-based commands.
+    /// </summary>
+    public class AsyncRelayCommand : ICommand
+    {
+        private readonly Func<object?, Task> _executeAsync;
+        private readonly Predicate<object?>? _canExecute;
+        private bool _isExecuting;
+
+        public AsyncRelayCommand(Func<object?, Task> executeAsync, Predicate<object?>? canExecute = null)
+        {
+            _executeAsync = executeAsync ?? throw new ArgumentNullException(nameof(executeAsync));
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object? parameter)
+        {
+            return !_isExecuting && (_canExecute == null || _canExecute(parameter));
+        }
+
+        public async void Execute(object? parameter)
+        {
+            if (!CanExecute(parameter))
+                return;
+
+            try
+            {
+                _isExecuting = true;
+                RaiseCanExecuteChanged();
+                await _executeAsync(parameter);
+            }
+            finally
+            {
+                _isExecuting = false;
+                RaiseCanExecuteChanged();
+            }
+        }
+
+        public event EventHandler? CanExecuteChanged;
+
+        public void RaiseCanExecuteChanged()
+        {
+            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
---
+++ RustServerMaster.UI/ViewModel/BaseViewModel.cs
@@ -0,0 +1,34 @@
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace RustServerMaster.ViewModel
+{
+  /// <summary>
+  /// Base class for all ViewModels, implements INotifyPropertyChanged.
+  /// </summary>
+  public class BaseViewModel : INotifyPropertyChanged
+  {
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    /// <summary>
+    /// Raise PropertyChanged event for the given property name.
+    /// </summary>
+    protected void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+    {
+      PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    /// <summary>
+    /// Set a backing field and raise PropertyChanged if the value actually changed.
+    /// </summary>
+    protected bool SetField<T>(ref T field, T value, [CallerMemberName] string? propertyName = null)
+    {
+      if (Equals(field, value))
+        return false;
+
+      field = value;
+      OnPropertyChanged(propertyName);
+      return true;
+    }
+  }
+}
